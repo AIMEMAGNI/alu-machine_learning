8d0a243f778d5441ab4c52ab651735aaed0c2f47
Aime Magnifique NDAYISHIMIYE <82208013+AIMEMAGNI@users.noreply.github.com>
2024-09-20 08:25:52 +0200

    done

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -5,4 +5,10 @@ import tensorflow as tf
 
 def create_train_op(loss, alpha):
     """ training operation"""
-    return tf.train.GradientDescentOptimizer(alpha).minimize(loss)
+    optimizer = tf.train.GradientDescentOptimizer(
+        learning_rate=alpha)
+
+    # Create the training operation
+    train_op = optimizer.minimize(loss)
+
+    return train_op
